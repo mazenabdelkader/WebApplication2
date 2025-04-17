18cf8a0d1892b90df1990ab9562188b5d9f06b36
A1 <A1@DESKTOP-98Q3DDS>
2025-04-17 19:44:37 +0200

    a

---
+++ WebApplication2/Controllers/employeecontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class employeecontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
